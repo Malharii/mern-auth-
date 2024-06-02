df894d8ed93405b046528c691d351dd029387d01
Malharii <mrmalhari96@gmail.com>
2024-06-02 14:21:14 +0530

    create header componets

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,10 +4,12 @@ import About from "./pages/About";
 import Singup from "./pages/Singup";
 import Signin from "./pages/Signin";
 import Profile from "./pages/Profile";
+import Header from "./componets/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
