777450a8320cb0078d8b9212703064ac3d60606c
Malharii <mrmalhari96@gmail.com>
2024-06-04 14:22:21 +0530

    create singuo and password hash

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import User from "../models/user.model.js";
-
+import bcryptjs from "bcryptjs";
 export const singup = async (req, res) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
+  const hashPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hashPassword });
   try {
     await newUser.save();
     res.status(201).json({ massage: "User created successfully" });
