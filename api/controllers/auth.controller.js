8db2681f2130192fc7fd5d03b888f3823756a5ed
Malharii <mrmalhari96@gmail.com>
2024-06-05 21:47:13 +0530

    complet signup functionlaty

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-export const singup = async (req, res, next) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({ username, email, password: hashPassword });
--- client/src/pages/Singup.jsx
+++ client/src/pages/Singup.jsx
@@ -1,30 +1,69 @@
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 function Singup() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+      if (data.success === false) {
+        setError(true);
+        return;
+      }
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Singup</h1>
-      <form className="flex flex-col gap-2">
+      <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
         <input
           type="text"
+          id="username"
           placeholder="username"
           className="my-2 bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           type="email"
+          id="email"
           placeholder="email"
           className="my-2 bg-slate-100 rounded-lg p-3"
+          onChange={handleChange}
         />
         <input
           type="password"
+          id="password"
           placeholder="password"
+          onChange={handleChange}
           className="my-2 bg-slate-100 rounded-lg p-3"
         />
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-slate-600 uppercase
+          disabled={loading}
+          className="bg-blue-500 hover:bg-blue-600 text-slate-600 uppercase
         hover:text-white font-bold py-2 px-4 rounded"
         >
-          Sing up
+          {loading ? "Loading..." : "Singup"}
         </button>
       </form>
       <div className="mt-4 flex gap-2">
@@ -33,6 +72,7 @@ function Singup() {
           <span className="text-blue-500 cursor-pointer">Sing in</span>
         </Link>
       </div>
+      <p className="text-red-500"> {error && "Something went wrong"}</p>
     </div>
   );
 }
