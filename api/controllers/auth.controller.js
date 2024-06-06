a4d2c5c746f810e8e2489fdbbde920ef738379a3
Malharii <mrmalhari96@gmail.com>
2024-06-06 18:28:51 +0530

    create sing api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const hashPassword = bcryptjs.hashSync(password, 10);
@@ -7,6 +10,38 @@ export const signup = async (req, res, next) => {
   try {
     await newUser.save();
     res.status(201).json({ massage: "User created successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validuser = await User.findOne({ email });
+    if (!validuser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validuser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Wrong password"));
+    }
+    const token = jwt.sign(
+      { id: validuser._id, email: validuser.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+    const { password: _, ...rest } = validuser._doc;
+    const expirydate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: expirydate,
+      })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
